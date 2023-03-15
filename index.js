ac629ed3a1282e7be7ad4ae267ba782d2fffa588
MORGANJASON <420morganjason@gmail.com>
2023-03-15 13:06:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+
+let companyName = 'Scuber'
+
+const companyName = 'Scuber';
+
+  let {
+   companyName = 'Skrr'
+  } return (err)
+    console.log(err);
+
+
+    let mostProfitableNeighborhood = 'Chelsea'
+
+
+    let companyCeo = 'Susan Smith'
