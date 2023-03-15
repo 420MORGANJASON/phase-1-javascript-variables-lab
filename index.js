15237188188e62ed434ded623ade2d95b25de312
MORGANJASON <420morganjason@gmail.com>
2023-03-15 13:57:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 // Code your solution in this file!
 
-let companyName = 'Scuber'
+// let companyName = 'Scuber'
 
 const companyName = 'Scuber';
 
-  let {
-   companyName = 'Skrr'
-  } return (err)
-    console.log(err);
+//  let mostProfitableNeighborhood = ('Chelsea')
+
+ var mostProfitableNeighborhood = ('Chelsea')
 
 
-    let mostProfitableNeighborhood = 'Chelsea'
+//  var companyCeo = ('Susan Smith')
 
-
-    let companyCeo = 'Susan Smith'
+ let companyCeo  = ('Susan Smith')
